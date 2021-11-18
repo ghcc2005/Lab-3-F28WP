4329a8ca495c1f2caa9903f7a1362fa062324d43
cc2005 <cc2005@hw.ac.uk>
2021-11-18 20:01:03 +0000

    Finished part 6 onto 7 havent done article.js yet

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+//creating app
+const app = express();
+//make the app listen on port
+const port = process.argv[2] || process.env.PORT || 3000;
+const server = app.listen(port, () => {
+ console.log(`Cart app listening at http://localhost:${port}`);
+});
+
+//send an HTTP response when receiving HTTP GET /
+//handling static HTML and EJS templates
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+ res.render('index'); //no need for ejs extension
+});
+
+//pass requests to the router middleware
+const router = require('./routes/apis');
+app.use(router);
+
+
+
+//route for contacts
+app.get('/contacts', (req, res) => {
+    res.render('contacts');
+   });
+
+//route for register
+app.get('/register', (req, res) => {
+    res.render('register');
+   });
+
+//route for login
+app.get('/login', (req, res) => {
+    res.render('login');
+   });
+
+
+
+
+
